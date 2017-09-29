69005e3837ee02daf6a28de1d4886180e7e20460
Paul Milbank <paulmilbank@gmail.com>
2017-09-29 21:11:48 +1300

    trim strings with multi char padding that go over requiredLength.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 function PadLeft (checkString, requiredLength, padChar = ' ') {
   checkString = checkString.toString()
   padChar = padChar.toString()
-  if (checkString.length >= requiredLength) {
+
+  if (checkString.length === requiredLength) {
     return checkString
+  } else if (checkString.length > requiredLength) {
+    let trimAmount = checkString.length - requiredLength
+    return checkString.slice(trimAmount)
   } else {
     let paddedString = `${padChar}${checkString}`
     return PadLeft(paddedString, requiredLength, padChar)
@@ -14,8 +18,12 @@ function PadLeft (checkString, requiredLength, padChar = ' ') {
 function PadRight (checkString, requiredLength, padChar = ' ') {
   checkString = checkString.toString()
   padChar = padChar.toString()
-  if (checkString.length >= requiredLength) {
+
+  if (checkString.length === requiredLength) {
     return checkString
+  } else if (checkString.length > requiredLength) {
+    let trimAmount = (requiredLength - checkString.length)
+    return checkString.slice(0, trimAmount)
   } else {
     let paddedString = `${checkString}${padChar}`
     return PadRight(paddedString, requiredLength, padChar)
